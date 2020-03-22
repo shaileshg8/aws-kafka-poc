4fa90c79fac71ade2d29c6763f27c541e965c512
Shailesh.Bishwakarma <shailesh.bishwakarma@apiture.com>
2020-03-22 02:11:55 -0500

    code refactoring

--- README.md
+++ README.md
@@ -7,10 +7,61 @@ pip install kafka-python
 
 Before installation please verify your aws credentials
 https://docs.aws.amazon.com/cli/latest/userguide/cli-configure-files.html
+```
 
+# Setting up Dynamo Db Table
+```
 Go to your AWS Management Console in your web browser
-Navigate to Dynamo Db service, and create a table called 'aws-kafka-topic-table'
+Navigate to Dynamo Db service, and create a table.
+Update the config.yml, with the name you provided or you can feed the lambda the dynamo db name (eg. event/kafkaTopic.json)
+{
+    "dynamo_db_name": "aws-kafka-topic-table",
+    "kfk_server": "localhost:9092",
+    "topic_name": "myTopic",
+    "student_id": "44"
+}
+
+
 Manually insert data with Partition Key , 'id'
+Example Sample data:
+{
+  "amount": 1200,
+  "end_date": "2020-03-22T04:36:10.418Z",
+  "id": "44",
+  "name": "test",
+  "start_date": "2020-03-22T04:36:10.418Z",
+  "state": "monthly"
+}
+
+If any of the data is missing in the dynamo db table, lambda will throw error
+
+```
+
+# Apache Kafka
+```
+Get the kafka server name and feed the lambda the server name and topic
+eg: event/kafkaTopic.json
+Sample:
+{
+    "dynamo_db_name": "aws-kafka-topic-table",
+    "kfk_server": "localhost:9092",
+    "topic_name": "myTopic",
+    "student_id": "44"
+}
+```
+
+# Response
+```
+Lamda will send a message to a kafka topic
+Sample Response data sedn to kafka topic:
+{
+  "amount": 1200,
+  "end_date": "2020-03-22T04:36:10.418Z",
+  "id": "44",
+  "name": "test",
+  "start_date": "2020-03-22T04:36:10.418Z",
+  "state": "monthly"
+}
 ```
 
 # Installation
@@ -21,6 +72,7 @@ npm i
 
 # Deployment
 ```
+npm run prepare
 npm run deploy
 ```
 
--- src/kafka-topic/index.py
+++ src/kafka-topic/index.py
@@ -1,6 +1,8 @@
-import boto3
 import json
 import os
+import sys
+sys.path.insert(0, 'src/vendor')
+import boto3
 from kafka import KafkaProducer
 
 # Helper class to convert a DynamoDB item to JSON.
@@ -23,6 +25,36 @@ def validate_event(event):
         return 'Error: Missing student_id in event'
     return 'ok'
 
+# Helper function to validate dynamo db item
+# if any of the requried field is missing it will throw error
+def validate_response(data):
+    if 'id' not in data:
+        return 'Error: Missing id in dynamo db table'
+    if 'name' not in data:
+        return 'Error: Missing name in dynamo db table'
+    if 'start_date' not in data:
+        return 'Error: Missing start_date in dynamo db table'
+    if 'end_date' not in data:
+        return 'Error: Missing end_date in dynamo db table'
+    if 'amount' not in data:
+        return 'Error: Missing amount in dynamo db table'
+    if 'state' not in data:
+        return 'Error: Missing state in dynamo db table'
+    return 'ok'
+
+# helper method to map dynamo db item to final response
+# if any more field is require add here
+def map_response(data):
+    response = {}
+    response['id'] = data['id']
+    response['name'] = data['name']
+    response['start_date'] = data['start_date']
+    response['end_date'] = data['end_date']
+    response['amount'] = data['amount']
+    response['state'] = data['state']
+    return response
+
+# Entry Method
 # Handler method for the lambda function
 def handler(event, context):
     result = 'ok'
@@ -32,12 +64,21 @@ def handler(event, context):
     if validate != 'ok':
         return validate
 
+    # Getting values from event passed to lambda
     kfk_server = event['kfk_server']
     topic_name = event['topic_name']
     student_id = event['student_id']
 
+    # Initializing dynamodb client
     client = boto3.client('dynamodb')
+    
+    #Getting table name from environment variable set in serverless yml
+    # To update the dynamo db name you can update config.yml file
     table_name = os.environ['DYNAMO_DB_NAME']
+
+    # if dynamo db name is fed to lambda through event, use this name
+    if 'dynamo_db_name' in event:
+        table_name = event['dynamo_db_name']
     
     try:
         # getting item from dynamo db table
@@ -47,9 +88,22 @@ def handler(event, context):
                 'id': {'S': student_id}
             }
         )
-        result=json.dumps(response, indent=2, cls=DecimalEncoder)
 
+        # validating the dynamo db item if requried data is missing
+        validate_item = validate_response(response['Item'])
+        if validate_item != 'ok':
+            return validate
+        
+        # map the dynamo db item to final response
+        # only required data is added, others are ignored
+        mapped_res = map_response(response['Item'])
+        result=json.dumps(mapped_res, indent=2, cls=DecimalEncoder)
+
+        # Creating a kafka client that publishes records
+        # it needs kfk_server fed to lambda in event
         producer = KafkaProducer(bootstrap_servers=[kfk_server])
+
+        # using producer to send the message to topic_name as provided in event
         send_message = producer.send(topic_name, result)
 
         try:
